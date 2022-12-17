bb6c894e6d8f0fc83bdc082c931fa84cfa2728c0
DmytroLis <dima8dima2@gmail.com>
2022-12-17 23:39:19 +0200

    +3 kata

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-function removeChar(str) {
-  return str.slice(1);
+// function removeChar(str) {
+//   return str.slice(1, -1);
+// }
+
+// let sre = removeChar('chai');
+// console.log(sre);
+
+// function basicOp(operation, value1, value2) {
+//   if (operation == '+') {
+//     res = value1 + value2;
+//   } else if (operation == '-') {
+//     res = value1 - value2;
+//   } else if (operation == '*') {
+//     res = value1 * value2;
+//   } else {
+//     res = value1 / value2;
+//   }
+//   return res;
+// }
+
+// res = basicOp('-', 15, 18);
+// console.log(res);
+
+function makeUpperCase(str) {
+  // Code here
+  res = str.toUpperCase();
+  return res;
 }
 
-let sre = removeChar('chai');
-console.log(sre);
+res = makeUpperCase('heloo');
+console.log(res);
