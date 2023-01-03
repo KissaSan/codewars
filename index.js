a49d7cdd652b49dff75fb24512f63d3f08bbf8df
DmytroLis <dima8dima2@gmail.com>
2023-01-03 14:28:24 +0200

    +3 kata

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-// function removeChar(str) {
-//   return str.slice(1, -1);
-// }
+function removeChar(str) {
+  return str.slice(1, -1);
+}
 
-// let sre = removeChar('chai');
-// console.log(sre);
+let sre = removeChar('chai');
+console.log(sre);
 
-// function basicOp(operation, value1, value2) {
-//   if (operation == '+') {
-//     res = value1 + value2;
-//   } else if (operation == '-') {
-//     res = value1 - value2;
-//   } else if (operation == '*') {
-//     res = value1 * value2;
-//   } else {
-//     res = value1 / value2;
-//   }
-//   return res;
-// }
+function basicOp(operation, value1, value2) {
+  if (operation == '+') {
+    res = value1 + value2;
+  } else if (operation == '-') {
+    res = value1 - value2;
+  } else if (operation == '*') {
+    res = value1 * value2;
+  } else {
+    res = value1 / value2;
+  }
+  return res;
+}
 
-// res = basicOp('-', 15, 18);
-// function makeUpperCase(str) {
-//   // Code here
-//   res = str.toUpperCase();
-//   return res;
-// }
+res = basicOp('-', 15, 18);
+function makeUpperCase(str) {
+  // Code here
+  res = str.toUpperCase();
+  return res;
+}
 
-// res = makeUpperCase('heloo');
-// console.log(res);
+res = makeUpperCase('heloo');
+console.log(res);
 
-// function twiceAsOld(dadYearsOld, sonYearsOld) {
-//   // your code here
-//   res =  Math.abs(dadYearsOld-(sonYearsOld * 2)) ;
-//   return res
-// }
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  // your code here
+  res = Math.abs(dadYearsOld - sonYearsOld * 2);
+  return res;
+}
 
 const stringToNumber = function (str) {
-  // put your code here
   return Number(str);
 };
 
 function lovefunc(flower1, flower2) {
   return flower1 % 2 !== flower2 % 2;
+}
+
+function hoopCount(n) {
+  return n < 10
+    ? 'Keep at it until you get it'
+    : 'Great, now move on to tricks';
 }
