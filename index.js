5ca84ef0e0644ac640861dd9813690d70ac6cbca
DmytroLis <dima8dima2@gmail.com>
2023-01-11 14:39:48 +0200

    +1 kata

--- index.js
+++ index.js
@@ -117,3 +117,17 @@ function removeEveryOther(arr){
  return arr.filter(function(_, index) {
     return !(index % 2);
   });}
+
+
+  function isIsogram(str){
+    let i, j;
+    str = str.toLowerCase();
+    for(i = 0; i < str.length; ++i) {
+      for(j = i + 1; j < str.length; ++j) {
+        if(str[i] === str[j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+ }
